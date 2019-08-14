994fc00f3b556b3e20b762a04d78dc9057942bc1
hmccabe2002 <45460078+hmccabe2002@users.noreply.github.com>
2019-08-14 14:00:00 -0500

    Update

--- script.js
+++ script.js
@@ -107,7 +107,7 @@ function createMemberElement(member) {
 }
 
 function updateMembersDOM() {
-  DOM.membersCount.innerText = `${members.length} users in room:`;
+  DOM.membersCount.innerHTML = `${members.length} users in room:`;
   DOM.membersList.innerHTML = '';
   members.forEach(member =>
     DOM.membersList.appendChild(createMemberElement(member))
